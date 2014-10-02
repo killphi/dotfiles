e92276d3e778e4a8a90e006dc159345de70ec089
killphi <killphi@gmx.de>
2014-10-02 13:54:18 +0200

    adding more fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,13 @@
 source /etc/fish/config.fish
+source ~/.config/fish/aliases.fish
+
+set -g fish_color_user green
+set -g fish_color_host cyan
+
+set -x PATH ~/.local/bin ~/bin $PATH ~/.rvm/bin ~/node_modules/.bin
+
+# this is fucked up
+#set -x LS_COLORS (dircolors ~/.dir_colors | cut -d\n -f1 | sed -e 's/.*\'\(.*\)\'.*/\1/' -e 's/:/ /g')
 
 set -x VISUAL vim
-set -x EDITOR $visual
-
-alias vi vim
+set -x EDITOR $VISUAL
