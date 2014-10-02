e5e75e72facebe35139eb845250b08049ae8a103
killphi <killphi@gmx.de>
2014-10-02 16:23:57 +0200

    fish prompt and tmux title fixes

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,7 +5,7 @@ source /etc/fish/config.fish
 source ~/.config/fish/aliases.fish
 
 # set path
-if status --is-login
+# if status --is-login
   if test -z (echo $PATH | grep $HOME/bin)
     set -x PATH ~/bin $PATH
   end
@@ -17,7 +17,7 @@ if status --is-login
   if test -z (echo $PATH | grep $HOME/node_moduls/.bin)
     set -x PATH $PATH ~/node_modules/.bin
   end
-end
+# end
 
 # VIm <3
 set -x VISUAL vim
@@ -27,9 +27,9 @@ set -x EDITOR $VISUAL
 set -g fish_color_user green
 set -g fish_color_host cyan
 
-# call rvm once, to set up env
-rvm ^&1 >/dev/null
-
 # dircolors
 # this is fucked up
 #set -x LS_COLORS (dircolors ~/.dir_colors | cut -d\n -f1 | sed -e 's/.*\'\(.*\)\'.*/\1/' -e 's/:/ /g')
+
+# call rvm once, to set up env
+rvm ^&1 >/dev/null
--- home/.config/fish/functions/fish_title.fish
+++ home/.config/fish/functions/fish_title.fish
@@ -1,9 +1,9 @@
 function fish_title
-  if not set -q __fish_title_home
-    set -g __fish_title_home (echo $HOME | sed 's/\\//\\\\\\//g')
+  if not set -q __home_escaped
+    set -g __home_escaped (echo $HOME | sed 's/\\//\\\\\\//g')
   end
 
-  set -l __fish_title (pwd | sed "s/^$__fish_title_home/~/")
+  set -l title (pwd | sed -e "s/^$__home_escaped/~/" -e 's/.*\/\(.*\/.*\)$/\1/')
 
-  tmux rename-window $__fish_title:$_
+  tmux rename-window $title:$_
 end
