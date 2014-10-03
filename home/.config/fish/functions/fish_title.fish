3ae428facfb0be046fdb572e3f183311006a7353
killphi <killphi@gmx.de>
2014-10-03 10:25:26 +0200

    fixed fish_title (ruby FTW)

--- home/.config/fish/functions/fish_title.fish
+++ home/.config/fish/functions/fish_title.fish
@@ -1,9 +1,3 @@
 function fish_title
-  if not set -q __home_escaped
-    set -g __home_escaped (echo $HOME | sed 's/\\//\\\\\\//g')
-  end
-
-  set -l title (pwd | sed -e "s/^$__home_escaped/~/" -e 's/.*\/\(.*\/.*\)$/\1/')
-
-  tmux rename-window $title:$_
+  tmux rename-window (pwd | ~/.config/fish/ext/dir_shortener.rb):$_
 end
