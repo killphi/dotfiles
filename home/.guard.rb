e2eda838de6a3d4e76fb2af31646bd64ea3a430a
killphi <killphi+git@gmail.com>
2014-10-27 13:36:15 +0100

    guard, rubocop and gitignore

--- home/.guard.rb
+++ home/.guard.rb
@@ -1,3 +1,5 @@
+interactor :off
+
 notification :tmux,
              display_message:        true,
              timeout:                3,
@@ -8,16 +10,19 @@ notification :tmux,
              # the first %s will show the title, the second the message
              color_location:         'status-left-fg'
 
-def has_plugin?(name)
-  PluginUtil::plugin_names.include? name
+if PluginUtil.plugin_names.include? 'bundler'
+  guard :bundler do
+    watch('Gemfile')
+  end
 end
 
-begin
-  require 'guardrc'
-  eval Guardrc.at '~/.guard/bundler.rb' if has_plugin? 'bundler'
-  eval Guardrc.at '~/.guard/rubocop.rb' if has_plugin? 'rubocop'
-rescue LoadError
-  nil
+if PluginUtil.plugin_names.include? 'rubocop'
+  guard :rubocop, all_on_start: false, cli: ['--format', 'simple'] do
+    watch(/.+\.rb$/)
+    watch('Rakefile')
+    watch('Guardfile')
+    watch('Gemfile')
+    watch('config.ru')
+    watch(/(?:.+\/)?\.rubocop(?:_todo)?\.yml$/) {|m| File.dirname(m[0]) }
+  end
 end
-
-#interactor :off
--- home/.guard/rubocop.rb
+++
@@ -1,8 +0,0 @@
-guard :rubocop do
-  watch(/.+\.rb$/)
-  watch('Rakefile')
-  watch('Guardfile')
-  watch('Gemfile')
-  watch('config.ru')
-  watch(/(?:.+\/)?\.rubocop\.yml$/) { |m| File.dirname(m[0]) }
-end
